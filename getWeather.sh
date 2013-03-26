1cc9e6d2e1f12ee259c0a68bb92c5d6731f50473
Denis Solonevich <denis.solonevich@gmail.com>
2013-03-26 13:55:19 +0300

    add some code

---
+++ getWeather.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ x$1 = x"--help" ] ; 
+then
+	echo "This script shows Minsk weather. You're able to config this script by config.ini file - just write a number there (in seconds)"
+	exit
+fi
+
+if ping -c1 weather.yahooapis.com >/dev/null;
+then
+	echo "Connection established!";
+else
+	echo "No connection!";
+	exit 1;
+fi
+
+TIME_TO_SLEEP=`cat config.ini`
+
+while true; do 
+	lwp-request "weather.yahooapis.com/forecastrss?w=834463&u=c" | grep -A1 "Current" | tail -n 1 | cut -d '<' -f1; 
+	sleep $TIME_TO_SLEEP; 
+done;
